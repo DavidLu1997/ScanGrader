f01edbd93377c9d3a8bcf470e21d83c7ae7dc7e2
Zopper9000 <david.lu@hotmail.com>
2015-08-24 19:49:59 -0400

    Added todos

--- AnalyzeImage.cpp
+++ AnalyzeImage.cpp
@@ -1,7 +1,12 @@
 #include "AnalyzeImage.h"
+//TODO: Config file API
+
 
 AnalyzeImage::AnalyzeImage(ScanImage s, std::string filename) {
-	
+	//Get image
+
+	//Load config file
+
 }
 
 std::vector<int> results() {
