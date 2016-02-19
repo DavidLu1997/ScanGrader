7294ba85282493180d8f97b5d5f32e1d5a2dc456
Zopper9000 <david.lu@hotmail.com>
2016-02-18 23:04:12 -0500

    deleted filelayout

--- guiElements/ExportWidget.cpp
+++ guiElements/ExportWidget.cpp
@@ -5,39 +5,32 @@ ExportWidget::ExportWidget(QWidget *parent) {
 	QWidget();
 
 	//Widgets
-	fileWidget = new QWidget();
-	fileLayout = new QGridLayout();
+	layout = new QGridLayout();
 
 	//Text
 	fileLabel = new QLabel("File Name: ");
-	fileLayout->addWidget(fileLabel, 0, 0);
+	layout->addWidget(fileLabel, 1, 0);
 	fileName = new QLineEdit();
 	fileName->setText("result");
-	fileLayout->addWidget(fileName, 0, 1);
+	layout->addWidget(fileName, 1, 1);
 	fileTypeLabel = new QLabel("File Type: ");
-	fileLayout->addWidget(fileTypeLabel, 1, 0);
+	layout->addWidget(fileTypeLabel, 2, 0);
 	fileType = new QComboBox();
 	fileType->addItem(".CSV");
 	fileType->addItem(".TXT");
-	fileLayout->addWidget(fileType, 1, 1);
+	layout->addWidget(fileType, 2, 1);
 
 	//Empties
 	QWidget *empty1 = new QWidget();
 	empty1->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-	fileLayout->addWidget(empty1, 999, 0);
+	layout->addWidget(empty1, 999, 0);
 	QWidget *empty2 = new QWidget();
 	empty2->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-	fileLayout->addWidget(empty2, 999, 1);
-
-	fileWidget->setLayout(fileLayout);
-
-	//Main widget
-	layout = new QGridLayout();
+	layout->addWidget(empty2, 999, 1);
 
 	intro = new QLabel("<h3>Export type:</h3>");
 	intro->setTextFormat(Qt::RichText); 
 	layout->addWidget(intro, 0, 0);
-	layout->addWidget(fileWidget, 2, 0);
 	QWidget *empty5 = new QWidget();
 	empty5->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 	layout->addWidget(empty5, 999, 0);
